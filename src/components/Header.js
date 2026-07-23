1b153fa9a12da664dc6add8b225c2ecaf5c4bc26
Akarshika Mehrotra <akarshikamehrotra44@gmail.com>
2026-07-23 21:54:04 +0530

    Few changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
-
   const [btnName, setbtnName] = useState("Login");
 
   const OnlineStatus = useOnlineStatus();
@@ -14,24 +13,26 @@ const Header = () => {
       <div className="logo-container">
         <img className="w-46" src={LOGO_URL} />
       </div>
+
       <div className="flex items-center">
         <ul className="flex p-4 m-4">
           <li className="p-4">Online Status : {OnlineStatus ? "✅" : "⚠️"}</li>
-          <li  className="p-4">
+          <li className="p-4">
             <Link to="/">HOME</Link>
           </li>
-          <li  className="p-4">
+          <li className="p-4">
             <Link to="/about">ABOUT US</Link>
           </li>
-          <li  className="p-4">
+          <li className="p-4">
             <Link to="/contact">CONTACT US</Link>
           </li>
-            <li  className="p-4">
+          <li className="p-4">
             <Link to="/grocery">GROCERY</Link>
           </li>
-          <li  className="p-4">
+          <li className="p-4">
             <Link to="/cart">CART</Link>
           </li>
+
           <button
             className="login"
             onClick={() => {
